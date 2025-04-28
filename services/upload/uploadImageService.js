605fdaa13658ce3d8c2ac90bbc29f1080dab7903
HauiZ <hauiz15122004@gmail.com>
2025-04-28 15:23:22 +0700

    delete storage cloudinary when update image

--- services/upload/uploadImageService.js
+++ services/upload/uploadImageService.js
@@ -5,31 +5,45 @@ import messages from "../../config/message.js";
 import cloudinary from '../../config/cloudinary.js';
 
 const uploadImageAndUpdate = async (file, userId, Model, updateFields) => {
-    if (!file) return { status: 400, data: { message: messages.file.ERR_FILE_NOT_EXISTS } };
+    try {
+        if (!file) return { status: 400, data: { message: messages.file.ERR_FILE_NOT_EXISTS } };
 
-    const public_id = file.filename || file.public_id;
-    const secure_url = file.path || file.secure_url;
-    if (!public_id || !secure_url) {
-        return { status: 400, data: { message: messages.file.UPLOAD_FAILED } };
+        const public_id = file.filename || file.public_id;
+        const secure_url = file.path || file.secure_url;
+        if (!public_id || !secure_url) {
+            return { status: 400, data: { message: messages.file.UPLOAD_FAILED } };
+        }
+        const user = await Model.findOne({ where: { userId } });
+
+        if (!user) {
+            return { status: 404, data: { message: messages.user.NOT_FOUND } };
+        }
+
+        if (user.logoId && user.logoId !== undefined && user.logoId !== null) {
+            await cloudinary.uploader.destroy(user.logoId);
+        }
+
+        if (user.avatarId && user.avatarId !== undefined && user.avatarId !== null) {
+            await cloudinary.uploader.destroy(user.avatarId);
+        }
+
+        await user.update({
+            [updateFields.id]: public_id,
+            [updateFields.url]: secure_url,
+        });
+
+        return {
+            status: 200,
+            data: {
+                message: messages.file.FILE_UPLOAD_ACCESS,
+                link: secure_url,
+            },
+        };
+    } catch (error) {
+        console.log(error)
+        return { status: 500, data: { message: messages.error.ERR_INTERNAL } };
     }
-    const user = await Model.findOne({ where: { userId } });
 
-    if (!user) {
-        return { status: 404, data: { message: messages.user.NOT_FOUND } };
-    }
-
-    await user.update({
-        [updateFields.id]: public_id,
-        [updateFields.url]: secure_url,
-    });
-
-    return {
-        status: 200,
-        data: {
-            message: messages.file.FILE_UPLOAD_ACCESS,
-            link: secure_url,
-        },
-    };
 };
 
 const uploadAvatar = (file, userId) => uploadImageAndUpdate(file, userId, PersonalUser, { id: 'avatarId', url: 'avatarUrl' });
